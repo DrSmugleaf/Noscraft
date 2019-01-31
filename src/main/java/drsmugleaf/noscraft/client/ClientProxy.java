c89e704db8ffcaa16a53bb58a73411cdb3929a6b
DrSmugleaf <javier.frayluis@gmail.com>
2019-01-31 22:13:26 +0100

    Created ClientProxy and CommonProxy classes and en_us.lang empty file

---
+++ src/main/java/drsmugleaf/noscraft/client/ClientProxy.java
@@ -0,0 +1,13 @@
+package drsmugleaf.noscraft.client;
+
+import drsmugleaf.noscraft.Noscraft;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+/**
+ * Created by DrSmugleaf on 31/01/2019
+ */
+@SideOnly(Side.CLIENT)
+@Mod.EventBusSubscriber(modid = Noscraft.MOD_ID, value = Side.CLIENT)
+public class ClientProxy {}
---
+++ src/main/java/drsmugleaf/noscraft/common/CommonProxy.java
@@ -0,0 +1,10 @@
+package drsmugleaf.noscraft.common;
+
+import drsmugleaf.noscraft.Noscraft;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Created by DrSmugleaf on 31/01/2019
+ */
+@Mod.EventBusSubscriber(modid = Noscraft.MOD_ID)
+public class CommonProxy {}
